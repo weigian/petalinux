3e13764f72a53082737b26493a3cdc6f3cc8e4fa
weigian <walter886@gmail.com>
2024-08-06 17:43:28 +0800

    script updated for added bit and hdf files

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+#!/bin/env sh
+#
+# https://github.com/z4yx/petalinux-docker/tree/master
+
+VER=2017.4
 #docker run -ti --rm -e DISPLAY=$DISPLAY --net="host" -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/vivado/.Xauthority -v /mnt/d/petalinux:/home/vivado/project  petalinux:${VER} /bin/bash
 #docker run -ti --rm -e DISPLAY=$DISPLAY --net="host" -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/vivado/.Xauthority -v .:/home/vivado/project  petalinux:${VER} /bin/bash
 docker run -ti --rm -e DISPLAY=$DISPLAY --net="host" -v /tmp/.X11-unix:/tmp/.X11-unix -v $HOME/.Xauthority:/home/vivado/.Xauthority -v .:/mnt  petalinux:${VER} /bin/bash
